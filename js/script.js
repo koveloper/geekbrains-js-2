af3c10c720557ad3fa62d7ce368fc1cc4902c603
koveloper <koveloper@gmail.com>
2021-03-15 23:46:38 +0300

    improve js code

--- js/script.js
+++ js/script.js
@@ -5,13 +5,22 @@ const goods = [
 	{ title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
+const renderGoodsItem = (title = 'new', price = '10') => {
 	return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 };
 
-const renderGoodsList = (list) => {
-	let goodsList = (list.map(item => renderGoodsItem(item.title, item.price)) + '').replaceAll(',', '');
-	document.querySelector('.goods-list').innerHTML = goodsList;
+const renderGoodsList = (list = []) => {
+	//variant-1
+	// let goodsList = (list.map(item => renderGoodsItem(item.title, item.price)) + '').replaceAll(',', '');
+	// document.querySelector('.goods-list').innerHTML = goodsList;
+	
+	//variant-2
+	// let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+	// goodsList.toString = function() {return this.reduce((acc, el) => {return acc + el;}, '');};
+	// document.querySelector('.goods-list').innerHTML = goodsList;
+
+	//variant-3
+	document.querySelector('.goods-list').innerHTML = list.reduce((acc, item) => {return acc + renderGoodsItem(item.title, item.price);}, '');
 };
 
 renderGoodsList(goods);
