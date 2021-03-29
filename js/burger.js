1ff10b7f0a9d6e293cb5f17cb540a4cd7e7cc39b
koveloper <65438466+koveloper@users.noreply.github.com>
2021-03-29 14:03:22 +0300

    Update burger.js
    rebase classes to use mostly javascript get/set

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const BurgerProperty = {
+    size: {
+        small: {price: 50, calories: 20, description: 'size SMALL'},
+        big: {price: 100, calories: 40, description: 'size XXL'}
+    },
+    stuff: {
+        cheese: {price: 10, calories: 20, description: 'CHEESE'},
+        salad: {price: 20, calories: 5, description: 'SALAD'},
+        potato: {price: 15, calories: 10, description: 'POTATO'}
+    },
+    components: {
+        spices: {price: 15, calories: 0, description: 'SPICES'},
+        mayo: {price: 20, calories: 5, description: 'MAYO'}
+    }
+}
+
 class BurgerComponent {
     constructor({price, calories, description}) {
         this.price = price;
@@ -7,16 +23,24 @@ class BurgerComponent {
         this.description = description;
     }
 
-    getPrice() {
-        return this.price;
+    set price(value) {
+        this._price = value;
     }
-
-    getCalories() {
-        return this.calories;;
+    
+    get price() {
+        return this._price;
+    }
+    
+    set calories(value) {
+        this._calories = value;
+    }
+    
+    get calories() {
+        return this._calories;;
     }
 
     toString() {
-        return this.description + ' (' + this.price + 'p' + ' / ' + this.calories + 'c)';
+        return this.description + ' (' + this._price + 'p' + ' / ' + this._calories + 'c)';
     }
 }
 
@@ -32,46 +56,46 @@ class Burger extends BurgerComponent {
     static COMPONENT_SPICES = {price: 15, calories: 0, description: 'SPICES'};
     static COMPONENT_MAYO = {price: 20, calories: 5, description: 'MAYO'};
     
-    constructor(size = Burger.SIZE_SMALL, stuff = Burger.STUFF_CHEESE) {
+    constructor(size = BurgerProperty.size.small, stuff = BurgerProperty.stuff.cheese) {
         super(size);
         this.components = [new BurgerComponent(stuff)];                
     }
 
-    addComponent(component = Burger.COMPONENT_SPICES) {
+    addComponent(component = BurgerProperty.components.spices) {
         this.components.push(new BurgerComponent(component));
         return this;
     }
 
-    getPrice() {
-        return super.getPrice() + this.components.reduce((acc, el) => {return acc + el.getPrice()}, 0);
+    get price() {
+        return super.price + this.components.reduce((acc, el) => {return acc + el.price}, 0);
     }
 
-    getCalories() {
-        return super.getCalories() + this.components.reduce((acc, el) => {return acc + el.getCalories()}, 0);    
+    get calories() {
+        return super.calories + this.components.reduce((acc, el) => {return acc + el.calories}, 0);    
     }
 
     toString() {
-        return super.toString() + ' | ' + this.components.join(' + ') + ": \r\n\t" + this.getPrice() + 'p / ' + this.getCalories() + 'c';
+        return super.toString() + ' | ' + this.components.join(' + ') + ": \r\n\t" + this.price + 'p / ' + this.calories + 'c';
     }
 
-    static getSizes() {
-        return [Burger.SIZE_SMALL, Burger.SIZE_BIG];
+    static get sizes() {
+        return [BurgerProperty.size.small, BurgerProperty.size.big];
     }
 
-    static getStuffs() {
-        return [Burger.STUFF_CHEESE, Burger.STUFF_SALAD, Burger.STUFF_POTATO];
+    static get stuffs() {
+        return [BurgerProperty.stuff.cheese, BurgerProperty.stuff.salad, BurgerProperty.stuff.potato];
     }
 
-    static getComponents() {
-        return [Burger.COMPONENT_SPICES, Burger.COMPONENT_MAYO];
+    static get components() {
+        return [BurgerProperty.components.spices, BurgerProperty.components.mayo];
     }
 }
 
-for(let size of Burger.getSizes()) {
-    for(let stuff of Burger.getStuffs()) {
+for(let size of Burger.sizes) {
+    for(let stuff of Burger.stuffs) {
         const burger = new Burger(size, stuff);
         console.log(burger.toString());
-        for(let stuff of Burger.getComponents()) {
+        for(let stuff of Burger.components) {
             burger.addComponent(stuff);
             console.log(burger.toString());
         }
